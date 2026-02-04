8e7e34341f09e95197a3d23486b007f5f665d3ad
Reb84 <152401972+reb84@users.noreply.github.com>
2026-02-04 13:48:41 +0000

    display image and hex

--- 11-colour-picker/script.js
+++ 11-colour-picker/script.js
@@ -1,4 +1,6 @@
 const submitBtn = document.getElementById("submit-btn");
+const displayScheme = document.getElementById("display-scheme");
+const displayHex = document.getElementById("display-hex");
 
 submitBtn.addEventListener("click", () => {
   const seedColor = document.getElementById("seed-colour").value.slice(1);
@@ -10,10 +12,25 @@ submitBtn.addEventListener("click", () => {
   getColors(seedColor, schemeValue);
 });
 
-getColors = (seedColor, schemeValue) => {
+const getColors = (seedColor, schemeValue) => {
   fetch(
     `https://www.thecolorapi.com/scheme?hex=${seedColor}&mode=${schemeValue}`,
   )
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => render(data));
+  // .then((data) => console.log(data));
+};
+
+const render = (data) => {
+  displayScheme.innerHTML = data.colors
+    .map((color) => {
+      return `<img src="${color.image.bare}">`;
+    })
+    .join("");
+
+  displayHex.innerHTML = data.colors
+    .map((hex) => {
+      return `<p>${hex.hex.value}</p>`;
+    })
+    .join("");
 };
